d1d7393343ecdeeb5db3b9142836c5cfbefb5137
GestaWeb16 <gestaweb16@gmail.com>
2024-08-21 11:41:22 -0500

    Initial commit

---
+++ src/app/superadmin/pages/list-superadmin/list-superadmin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-superadmin',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-superadmin.component.html',
+  styleUrl: './list-superadmin.component.scss'
+})
+export class ListSuperadminComponent {
+
+}
