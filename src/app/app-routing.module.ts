e2ad8aba566553f8800a7af79c5008ab39c38bab
GestaWeb16 <lindakather1324@gmail.com>
2024-08-23 17:36:32 -0500

    LM-organizacion-plantilla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './theme/layout/admin/admin.component';
-import { GuestComponent } from './theme/layout/guest/guest.component';
+import { MenuComponent } from './superadmin/pages/menu/menu.component';
+import { LoginComponent } from './auth/pages/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AdminComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: '/default',
-        pathMatch: 'full'
-      },
-      {
-        path: 'default',
-        loadComponent: () => import('./demo/default/default.component').then((c) => c.DefaultComponent)
-      },
-      {
-        path: 'typography',
-        loadComponent: () => import('./demo/elements/typography/typography.component')
-      },
-      {
-        path: 'color',
-        loadComponent: () => import('./demo/elements/element-color/element-color.component')
-      },
-      {
-        path: 'sample-page',
-        loadComponent: () => import('./demo/sample-page/sample-page.component')
-      }
-    ]
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '',
-    component: GuestComponent,
+    component: MenuComponent,
     children: [
       {
-        path: 'guest',
-        loadChildren: () => import('./demo/pages/authentication/authentication.module').then((m) => m.AuthenticationModule)
+        path: 'dashboard',
+        loadComponent: () => import('./superadmin/pages/dashboard/dashboard.component').then((c) => c.DefaultComponent)
       }
     ]
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './theme/layout/admin/admin.component';
-import { NavigationItem } from './theme/layout/admin/navigation/navigation';
-import { NavBarComponent } from './theme/layout/admin/nav-bar/nav-bar.component';
-import { NavLeftComponent } from './theme/layout/admin/nav-bar/nav-left/nav-left.component';
-import { NavRightComponent } from './theme/layout/admin/nav-bar/nav-right/nav-right.component';
-import { NavigationComponent } from './theme/layout/admin/navigation/navigation.component';
-import { NavLogoComponent } from './theme/layout/admin/nav-bar/nav-logo/nav-logo.component';
-import { NavContentComponent } from './theme/layout/admin/navigation/nav-content/nav-content.component';
-import { NavGroupComponent } from './theme/layout/admin/navigation/nav-content/nav-group/nav-group.component';
-import { NavCollapseComponent } from './theme/layout/admin/navigation/nav-content/nav-collapse/nav-collapse.component';
-import { NavItemComponent } from './theme/layout/admin/navigation/nav-content/nav-item/nav-item.component';
+import { NavigationItem } from './superadmin/pages/menu/navigation/navigation';
+import { NavBarComponent } from './superadmin/pages/menu/nav-bar/nav-bar.component';
+import { NavLeftComponent } from './superadmin/pages/menu/nav-bar/nav-left/nav-left.component';
+import { NavRightComponent } from './superadmin/pages/menu/nav-bar/nav-right/nav-right.component';
+import { NavigationComponent } from './superadmin/pages/menu/navigation/navigation.component';
+import { NavLogoComponent } from './superadmin/pages/menu/nav-bar/nav-logo/nav-logo.component';
+import { NavContentComponent } from './superadmin/pages/menu/navigation/nav-content/nav-content.component';
+import { NavGroupComponent } from './superadmin/pages/menu/navigation/nav-content/nav-group/nav-group.component';
+import { NavCollapseComponent } from './superadmin/pages/menu/navigation/nav-content/nav-collapse/nav-collapse.component';
+import { NavItemComponent } from './superadmin/pages/menu/navigation/nav-content/nav-item/nav-item.component';
 import { SharedModule } from './theme/shared/shared.module';
-import { ConfigurationComponent } from './theme/layout/admin/configuration/configuration.component';
 import { GuestComponent } from './theme/layout/guest/guest.component';
-
+import { SpinnerComponent } from './theme/shared/components/spinner/spinner.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,8 +31,8 @@ import { GuestComponent } from './theme/layout/guest/guest.component';
     NavGroupComponent,
     NavItemComponent,
     NavCollapseComponent,
-    ConfigurationComponent,
-    GuestComponent
+    GuestComponent,
+    SpinnerComponent
   ],
   imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
   providers: [NavigationItem],
